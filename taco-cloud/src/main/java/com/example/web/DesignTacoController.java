8ac5540afab1f441eff1a662749809f0b3b9d436
jaewoopk <jaewoopk000@naver.com>
2023-03-10 15:30:12 +0900

    Day 4 -

--- taco-cloud/src/main/java/com/example/web/DesignTacoController.java
+++ taco-cloud/src/main/java/com/example/web/DesignTacoController.java
@@ -1,56 +1,56 @@
-package com.example.web;
+// package com.example.web;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+// import java.util.Arrays;
+// import java.util.List;
+// import java.util.stream.Collectors;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.ui.Model;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.tacocloud.Ingredient;
-import com.example.tacocloud.Taco;
-import com.example.tacocloud.Ingredient.Type;
+// import com.example.tacocloud.Ingredient;
+// import com.example.tacocloud.Taco;
+// import com.example.tacocloud.Ingredient.Type;
 
-import lombok.extern.slf4j.Slf4j;
+// import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
-@Controller
-@RequestMapping("/design")
-public class DesignTacoController {
+// @Slf4j
+// @Controller
+// @RequestMapping("/design")
+// public class DesignTacoController {
     
-    @GetMapping
-    public String showDesignForm(Model model) {
-        List<Ingredient> ingredients = Arrays.asList(
-            new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-            new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-            new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-            new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-            new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-            new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-            new Ingredient("CHED", "Cheddar", Type.CHEESE),
-            new Ingredient("JACK", "Monterrey", Type.CHEESE),
-            new Ingredient("SLSA", "Salsa", Type.SAUCE),
-            new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-        );
+//     @GetMapping
+//     public String showDesignForm(Model model) {
+//         List<Ingredient> ingredients = Arrays.asList(
+//             new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+//             new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
+//             new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
+//             new Ingredient("CARN", "Carnitas", Type.PROTEIN),
+//             new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
+//             new Ingredient("LETC", "Lettuce", Type.VEGGIES),
+//             new Ingredient("CHED", "Cheddar", Type.CHEESE),
+//             new Ingredient("JACK", "Monterrey", Type.CHEESE),
+//             new Ingredient("SLSA", "Salsa", Type.SAUCE),
+//             new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
+//         );
 
-        Type[] types = Ingredient.Type.values();
-        for (Type type : types) {
-            model.addAttribute(type.toString().toLowerCase(),
-                filterByType(ingredients, type));
-        }
+//         Type[] types = Ingredient.Type.values();
+//         for (Type type : types) {
+//             model.addAttribute(type.toString().toLowerCase(),
+//                 filterByType(ingredients, type));
+//         }
 
-        model.addAttribute("taco", new Taco());
+//         model.addAttribute("taco", new Taco());
 
-        return "design";
-    }
+//         return "design";
+//     }
 
-private List<Ingredient> filterByType(
-    List<Ingredient> ingredients, Type type) {
-        return ingredients
-                    .stream()
-                    .filter(x -> x.getType().equals(type))
-                    .collect(Collectors.toList());
-    }
-}
+// private List<Ingredient> filterByType(
+//     List<Ingredient> ingredients, Type type) {
+//         return ingredients
+//                     .stream()
+//                     .filter(x -> x.getType().equals(type))
+//                     .collect(Collectors.toList());
+//     }
+// }
